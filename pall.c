bd4d9aaa309b558a9409e1526c39e3061450a2fe
Bbtee01 <111113241+Bbtee01@users.noreply.github.com>
2022-12-17 21:26:22 +0100

    11

---
+++ pall.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	print_stack(*stack);
+}
